6e22ba7691cc9197b52e591995b3c4e482e2fda0
manuhanono <mhanono@itba.edu.ar>
2023-12-12 12:49:30 -0300

    funciona todo menos coments, faltan arreglar cosas

--- resources/web/js/home.js
+++ resources/web/js/home.js
@@ -1,70 +1,99 @@
-// peliculas populares
-let url = `https://api.themoviedb.org/3/movie/popular?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b`;
+function realizarBusqueda() {
+        console.log('Entrando en realizarBusqueda');
+        const apiGatewayUrl = 'https://1kpho6lka1.execute-api.us-east-1.amazonaws.com/dev/myresource';
 
-console.log(url)
+        
+        // Obtiene el valor del campo de búsqueda
+        const valorBuscador = 'Freelance';
+        console.log('Valor del buscador:', valorBuscador);
+    
+        // Parámetros de búsqueda
+        const buscador = valorBuscador;
+        const media = 'movies';
+    
+        // Construye la URL con los parámetros
+        const apiUrl = `${apiGatewayUrl}?buscador=${buscador}&media=${media}`;
+        console.log('URL de la API:', apiUrl);
+    
+        // Realiza la solicitud utilizando Fetch API
+        fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('Datos de la Lambda:', data);
+            
+             // Accede a los valores específicos de la respuesta JSON
+             const posterPath = data.html[0]["Poster Path"].S;
+             const titulo = data.html[0].Nombre.S;
+             const sinopsis = data.html[0].Sinopsis.S;
+     
+             // Asigna los valores a los elementos HTML
+             let imagen = document.querySelector(".imagen");
+             let tituloElemento = document.querySelector(".titulo");
+             let sinopsisElemento = document.querySelector(".sinopsis");
+     
+             // Asigna los valores a los elementos HTML
+             imagen.src = posterPath;
+             tituloElemento.innerHTML = titulo;
+             sinopsisElemento.innerHTML = sinopsis;
+    
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+// Definir los valores constantes para buscadorInput
 
-fetch(url)
+
+document.addEventListener('DOMContentLoaded', realizarBusqueda);
+
+function realizarBusqueda2() {
+    console.log('Entrando en realizarBusqueda');
+    const apiGatewayUrl = 'https://1kpho6lka1.execute-api.us-east-1.amazonaws.com/dev/myresource';
+
+    
+    // Obtiene el valor del campo de búsqueda
+    const valorBuscador = 'Reign of Chaos';
+    console.log('Valor del buscador:', valorBuscador);
+
+    // Parámetros de búsqueda
+    const buscador = valorBuscador;
+    const media = 'movies';
+
+    // Construye la URL con los parámetros
+    const apiUrl = `${apiGatewayUrl}?buscador=${buscador}&media=${media}`;
+    console.log('URL de la API:', apiUrl);
+
+    // Realiza la solicitud utilizando Fetch API
+    fetch(apiUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        let info = data.results;
-        console.log(data.results);
-        let peliculas = ""
-        let capturo = document.querySelector(".padre-peliculas-populares-home")
-        console.log(capturo)
-        for (let i = 0; i < 4; i++) {
-            peliculas += `    
-                    <article class="peli-home">
-                    <a href="detailmovie.html?movie_id=${info[i].id}">
-                         <img class="imgpeli-home" src="https://image.tmdb.org/t/p/w342${info[i].poster_path}"  alt="">
-                         </a>
-                        <h3 class="h3-home"> ${info[i].title}</h3>
-                        <p class="fecha-estreno-home"> Fecha de estreno: ${info[i].release_date}<p>
-                    </article>`
-
-        }
-
-        capturo.innerHTML = peliculas;
+        console.log('Datos de la Lambda:', data);
+        
+         // Accede a los valores específicos de la respuesta JSON
+         const posterPath = data.html[0]["Poster Path"].S;
+         const titulo = data.html[0].Nombre.S;
+         const sinopsis = data.html[0].Sinopsis.S;
+ 
+         // Asigna los valores a los elementos HTML
+         let imagen = document.querySelector(".imagen2");
+         let tituloElemento = document.querySelector(".titulo2");
+         let sinopsisElemento = document.querySelector(".sinopsis2");
+ 
+         // Asigna los valores a los elementos HTML
+         imagen.src = posterPath;
+         tituloElemento.innerHTML = titulo;
+         sinopsisElemento.innerHTML = sinopsis;
 
     })
-
-
     .catch(function (error) {
-        console.log("Error: " + error)
+        console.log(error);
     })
-
-// pelis mas vistas
-let url3 = `https://api.themoviedb.org/3/movie/top_rated?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b`;
-
-console.log(url3)
-
-fetch(url3)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        let info = data.results;
-        console.log(data.results);
-        let pelipopu = ""
-        let capturo = document.querySelector(".padre-mas-visto-serie-home")
-
-        for (let i = 0; i < 4; i++) {
-            pelipopu += `    
-                    <article class="peli-mas-vista-home">
-                    <a href="detailmovie.html?movie_id=${info[i].id}">
-                         <img class="img-mas-vistas-home" src="https://image.tmdb.org/t/p/w342${info[i].poster_path}"  alt="">
-                         </a>
-                        <h3 class="h3-home"> ${info[i].title}</h3>
-                        <p class="fecha-estreno-home"> Fecha de estreno: ${info[i].release_date}<p>
-                    </article>`
-        }
-
-        capturo.innerHTML = pelipopu;
-
-    })
+}
+// Definir los valores constantes para buscadorInput
 
 
-    .catch(function (error) {
-        console.log("Error: " + error)
-    })
+document.addEventListener('DOMContentLoaded', realizarBusqueda2);
